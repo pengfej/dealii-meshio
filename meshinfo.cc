b6d30f3969f5e4aa28ef3f8efc0ef6bac3faf54a
pengfej <pengfej@clemson.edu>
2021-09-09 17:18:28 -0400

    initial setup complete.

--- meshinfo.cc
+++ meshinfo.cc
@@ -12,109 +12,12 @@
 using namespace dealii;
 
 
-void help_menu(){
-
-
-  std::cout <<"Usage:     ./meshinfo <arg1> <arg2>" << std::endl;
-  std::cout <<"           ./meshinfo <arg>" << std::endl;
-
-  std::cout <<"Options: " << std::endl;
-
-  std::cout << "                      -i   <filename>  Input file." << std::endl;
-  std::cout << "                      -o   <filename>  Output file." << std::endl;
-  std::cout << "                      -id  <dim>       Expected input dimension.  " << std::endl;
-  std::cout << "                      -od  <dim>       Expected output dimension. " << std::endl;
-  std::cout << "                      -r   <refine#>   How many refine times are expected." << std::endl;
-  std::cout << "                      -b               Extract boundary and output boundary ID" << std::endl;
-
-  
-  std::cout << "\n " << std::endl;
-  std::cout <<"When entering output file, make sure you enter the entire file name. \n" << 
-              "Like 'example.vtu' instead of 'example' or 'vtu', this way the nameing \n" <<
-              "of the output file is user defined so that it won't bother other files in the folder.\n" << std::endl;
-  std::cout << "\n" << std::endl;
-  std::cout << "dimension is by default, 2." << std::endl;
-
-}
-
-
-template <int dim>
-Triangulation<dim> reading_file(const std::string & input_file)
-{
-
-  const Triangulation<dim> &triangulation;
-
-  int pos_dot = input_file.find(".");
-
-  std::string input_file_type = input_file.substr(pos_dot+1, input_file.length()-1);
-  
-  std::cout << "input file type is " << input_file_type << std::endl;
-
-  GridIn<dim> gridin;
-
-  gridin.attach_triangulation(triangulation);
-
-  std::ifstream f(input_file);
-
-  if (input_file_type.compare("vtu")){
-
-    gridin.read_vtu(f);
-
-  } else if (input_file_type.compare("msh"))
-
-  {
-   
-    gridin.read_msh(f);
-
-  }
-
-  return triangulation;
-
-}
-
-
-
-template <int dim>
-void writing_file(const Triangulation<dim> &triangulation,const std::string & output_file)
-{
-
-  int pos_dot = output_file.find(".");
-
-  std::string output_file_type = output_file.substr(pos_dot+1, output_file.length()-1);
-  
-  std::cout << "Output file type is " << output_file_type << std::endl;
-
-  std::ofstream out(output_file);
-  GridOut       grid_out;
-  
-  if (output_file_type.compare("vtu")){
-
-    grid_out.write_vtu(triangulation, out);
-
-  } else if (output_file_type.compare("msh"))
-
-  {
-
-      grid_out.write_msh(triangulation, out);
-      
-  }
-  
-}
-
-
-
-void refine_grid(int refine_times){
-    
-    
-    
-}
-
 template<int dim>
 void bound_func(const Triangulation<dim> &triangulation){
   std::cout << "Mesh info:" << std::endl
             << " dimension: " << dim << std::endl
             << " no. of cells: " << triangulation.n_active_cells() << std::endl;
-            
+
   {
   std::map<types::boundary_id, unsigned int> boundary_count;
   for (const auto &face : triangulation.active_face_iterators())
@@ -128,47 +31,108 @@ void bound_func(const Triangulation<dim> &triangulation){
     }
   std::cout << std::endl;
   }
-    
+
 }
 
 
+void help_menu(){
 
 
+  std::cout <<"Usage:     ./meshinfo <arg1> <arg2>" << std::endl;
+  std::cout <<"           ./meshinfo <arg>" << std::endl;
+
+  std::cout <<"Options: " << std::endl;
+
+  std::cout << "                      -i   <filename>  Input file." << std::endl;
+  std::cout << "                      -o   <filename>  Output file." << std::endl;
+  std::cout << "                      -id  <dim>       Expected input dimension. (not ready yet)  " << std::endl;
+  std::cout << "                      -od  <dim>       Expected output dimension. (not ready yet)" << std::endl;
+  std::cout << "                      -r   <refine#>   How many refine times are expected. (not ready yet)" << std::endl;
+  std::cout << "                      -b               Extract boundary and output boundary ID. (not ready yet)" << std::endl;
 
 
+  std::cout << "\n " << std::endl;
+  std::cout <<"When entering output file, make sure you enter the entire file name. \n" <<
+              "Like 'example.vtu' instead of 'example' or 'vtu', this way the nameing \n" <<
+              "of the output file is user defined so that it won't bother other files in the folder.\n" << std::endl;
+  std::cout << "\n" << std::endl;
+
+}
 
 
+void convert_type(const std::string output_file, const std::string input_file, bool print_bound){
+
+  Triangulation<2> triangulation;
+
+  int pos_dot = input_file.find(".");
+
+  std::string input_file_type = input_file.substr(pos_dot+1, input_file.length()-1);
+
+  std::cout << "input file type is " << input_file_type << std::endl;
+
+  GridIn<2> gridin;
+
+  if (input_file_type.compare("vtu") == 0){
+
+    std::ifstream f(input_file);
+
+    gridin.read_vtu(f);
+
+    //keeping this error here intionally, so I don't to write the error myself
+    //:P
+
+  } else if (input_file_type.compare("msh") == 0)
+
+  {
+
+    gridin.attach_triangulation(triangulation);
+
+    std::ifstream f(input_file);
+
+    gridin.read_msh(f);
+
+  }
+
+  if (print_bound){
+
+    bound_func(triangulation);
+
+  }
+
+  pos_dot = output_file.find(".");
+
+  std::string output_file_type = output_file.substr(pos_dot+1, output_file.length()-1);
+
+  std::cout << "Output file type is " << output_file_type << std::endl;
+
+  std::ofstream out(output_file);
+  GridOut       grid_out;
+
+  if (output_file_type.compare("vtu") == 0){
+
+    grid_out.write_vtu(triangulation, out);
+
+  } else if (output_file_type.compare("msh") == 0)
+
+  {
+
+    grid_out.write_msh(triangulation, out);
+
+  }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//main!
-
-
+}
 
 int main(int argc, char *argv[]){
-    
+
   bool input_flag = false;
-  bool out_dim_flag = false;
-  bool in_dim_flag = false;
   bool output_flag = false;
   bool bound_flag = false;
   bool fine_flag = false;
-  
-  int dimension = 2;
-  int refine_times = 1;
+
+  std::string input_file = " ";
+  std::string output_file = " ";
+  int refine_times = 0;
 
   int i = 1;
 
@@ -184,47 +148,29 @@ int main(int argc, char *argv[]){
         help_menu();
 
       } else if (arg == "-o"){
-          
+
         output_flag = true;
 
-        std::string output_file = argv[i];
+        output_file = argv[i];
+        //std::cout << "output file name is " << output_file << std::endl;
 
         ++i;
 
       } else if (arg == "-i") {
-          
+
         input_flag = true;
-        
-        std::string input_file = argv[i];
-        
+
+        input_file = argv[i];
+        //std::cout << "input file name is " << input_file << std::endl;
+
         ++i;
-
-      } else if (arg == "-id"){
-
-        in_dim_flag = true;
-        
-        const int in_dim = atoi(argv[i]);
-        
-        ++i;
-        
-        std::cout << "The dimension of input file is: " << in_dim << std::endl;
-
-      } else if (arg == "-od"){
-
-        out_dim_flag = true;
-        
-        const int out_dim = atoi(argv[i]);
-        
-        ++i;
-        
-        std::cout << "The dimension of output file is: " << out_dim << std::endl;
 
       } else if (arg == "-r"){
-          
+
         fine_flag = true;
 
         refine_times = atoi(argv[i]);
-        
+
         ++i;
 
       } else if (arg == "-b"){
@@ -242,40 +188,30 @@ int main(int argc, char *argv[]){
     }
 
   }
-  
-  
-  //If you don't read then you don't write.
-  
-  if (input_flag){
-      
-      switch(in_dim){
-          
-          case 2:
-              
-          case 3:
-              
-      }
-      
-      //Other operations will be plugged in this section. like refine and boundary output.
-      
-  
-      if (output_flag){
-      
-          switch(out_dim){
-              
-              case 2:
-                  
-              case 3:
-              
-          }
-          
-      
-    }
-  
-  } 
-  
 
- 
+
+
+  if (input_flag && output_flag && bound_flag){
+
+      convert_type(output_file, input_file, true);
+      std::cout << "file converted and saved." << std::endl;
+
+  } else if (input_flag && output_flag && !bound_flag){
+
+      convert_type(output_file, input_file, false);
+      std::cout << "file converted and saved." << std::endl;
+
+  } else {
+
+      std::cout << "Not enough input argument, please check help menu." << std::endl;
+
+      help_menu();
+
+  }
+
+
+
+
 
   return EXIT_SUCCESS;
 
